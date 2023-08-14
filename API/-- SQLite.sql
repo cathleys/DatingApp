7f787500505cc37dc95d6d99507f80f622f6db7b
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-08-14 16:45:12 +0800

    Creates database and url endpoints

---
+++ API/-- SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Keanu");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Sandra");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Ryan");
+    
